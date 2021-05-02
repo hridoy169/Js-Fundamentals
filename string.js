9e169efbba0f25600cf79093a46599689d5a23f7
hridoy169 <46601095+hridoy169@users.noreply.github.com>
2021-05-02 14:52:29 +0600

    Add files via upload

---
+++ string.js
@@ -0,0 +1,63 @@
+let str1 = "This is string "
+let str2 = "a difficult concept"
+
+// //chartAt()
+// console.log(str1.charAt(0)) //return a character from a string
+// console.log(str1.charAt(1))
+
+// //charCode
+// console.log(str1.charCodeAt(0)) //returns a number indicating the Unicode value of the character
+// console.log(str1.charCodeAt(1))
+
+// //concat
+// console.log(str1.concat(str2)) //concat between two strings
+
+// //indexOf
+// console.log(str1.indexOf("string")) //returns the index of (the position of) the first occurrence of a specified text in a string
+
+// //lastIndexOf
+// let str3 = "This shit is truly a shit"
+// console.log(str3.lastIndexOf("shit")) //returns the index of (the position of) the last occurrence of a specified text in a string
+
+// //Both indexOf(), and lastIndexOf() return -1 if the text is not found.
+
+// //length
+// console.log(str1.length)
+
+// //search
+// console.log(str1.search("string")) 
+
+// //slice
+// console.log(str1.slice(8, 14)) //slice() extracts a part of a string and returns the extracted part in a new string.
+
+// console.log(str1.slice(-7, -1)) //If a parameter is negative, the position is counted from the end of the string.
+// console.log(str1.slice(8)) //If you omit the second parameter, the method will slice out the rest of the string.
+
+// //substring
+// let str = "Apple, Banana, Kiwi";
+// console.log(str.substring(7, 13)) // substr cannot take neg parameters
+
+// //substr
+// let str = "Apple, Banana, Kiwi";
+// console.log(str.substr(7, 6)) //second parameter specifies the length of the extracted part.
+
+// //replace
+// let str = "Apple, Banana, Kiwi";
+// console.log(str.replace("Banana", "Guava")) // replace a specified value with another value in a string.
+
+// //uppercase
+// console.log(str1.toUpperCase())
+
+// //loweercase
+// console.log(str1.toLowerCase())
+
+//trim
+let s = "Fuck off           "
+console.log(s.trim()) //remove whie space from both side of the strings
+
+
+
+
+
+
+
